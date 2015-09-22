3882bf870956a627c77a7929772d00d18e5f08dd
Bruno Bonnin <bbonnin@gmail.com>
2015-09-22 19:15:45 +0200

    gestion des events

--- quizz-server.js
+++ quizz-server.js
@@ -7,7 +7,7 @@ var express = require('express'),
 
 var users = {};
 var quizzStarted = false;
-var timeout = undefined;
+var timeoutQuizzStart = undefined;
 var questionId = 0;
 
 // Express configuration
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 //
 var httpServer = http.Server(app);
 httpServer.listen(port, function() {
-    console.log('You can test by opening: "localhost:' + port + '"');
+    console.log('*** You can test by opening: "localhost:' + port + '" ***');
 });
 
 // Socket IO processing
@@ -29,8 +29,29 @@ var io = require('socket.io').listen(httpServer);
 
 io.on('connection', function(socket) {
 
+    // Events received by the server and the emission of events :
+    //  - user connect (P1)
+    //    >> quizz started (P1)
+    //    >> quizz not started (P1)
+    //    >> user list (P2)
+    //  - user disconnect (P2)
+    //    >> user list (P2)
+    //  - user answer (P1)
+    //
+    // Other emitted events:
+    //  - The quizz starts:
+    //    >> quizz started
+    //  - New question sent to players:
+    //    >> new question
+    //    >> question timeleft
+    //    >> question answer
+    //    >> users answers
+    //  - End of the game:
+    //    >> end quizz
+
+
     socket.on('user connect', function(nickname) {
-        console.log('new user : ' + nickname);
+        console.log(' * new user : ' + nickname);
         socket.nickname = nickname;
         users[nickname] = { nickname : nickname, score : 0, socketId: socket.id };
 
@@ -42,18 +63,18 @@ io.on('connection', function(socket) {
         else {
             if (_.size(users) == 1) {
                 // Start a timer, after the end of it, the quizz will start
-                timeout = setTimeout(startQuizz, 10000);    
+                timeoutQuizzStart = setTimeout(startQuizz, 10000);    
             }
 
             socket.emit('quizz not started', {
                 msg: 'Le quizz va bienôt démarrer, merci de patienter',
                 users: users,
-                startIn:  getTimeLeft(timeout) });
+                startIn:  getTimeLeft(timeoutQuizzStart) });
         }
     });
 
     socket.on('user disconnect', function(nickname) {
-        console.log('user left : ' + nickname);
+        console.log(' * user left : ' + nickname);
         delete users[nickname];
         socket.broadcast.emit('user list', {
             users: users
@@ -63,15 +84,20 @@ io.on('connection', function(socket) {
             reinitQuizz();
         }
     });
+
+    socket.on('user answer', function (userAnswer) {
+        console.log(' * user answer : ' + userAnswer);
+    });
+
 });
 
 //----------------------------------------------
 // Quizz management functions
 //----------------------------------------------
 function reinitQuizz() {
-    console.log('reinit quizz');
+    console.log(' * reinit quizz');
     quizzStarted = false;
-    timeout = undefined;
+    timeoutQuizzStart = undefined;
     questionId = 0;
 }
 
@@ -81,7 +107,7 @@ function startQuizz() {
         msg: 'Le quizz démarre !'
     });
 
-    setTimeout(sendQuestion, 5000);
+    questionTimeout = setTimeout(sendQuestion, 5000);
 }
 
 function sendQuestion() {
@@ -93,12 +119,26 @@ function sendQuestion() {
             { response: 'une autre réponse (moins belle)' },
             { response: 'la bonne réponse' }
         ],
-        // Bad parameters :-)
-        goodResponse: 2,
-        wikipediaUrl: 'https://fr.wikipedia.org/w/api.php?action=query&format=json&titles=Jules%20Verne&prop=extracts&explaintext=true&exintro=true'
+        timeleft: 10
     });
+
+    setTimeout(sendQuestionTimeLeft, 1000);
 }
 
+function sendQuestionTimeLeft() {
+    var timeleft = getTimeLeft(questionTimeout);
+    if (timeleft <= 0) {
+        io.sockets.emit('question answer', {
+            id: 1,
+            link: '',
+            streetView: ''  
+        });
+    }
+    else {
+        io.sockets.emit('question timeleft', { timeleft : timeleft });
+    }
+    
+}
 
 //----------------------------------------------
 // Utilities functions
