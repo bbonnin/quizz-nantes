2abb574e21199156e6b06f3c7fef9c8e856aaded
padewitte <pad@myscript.com>
2015-09-23 23:44:06 +0200

    Adding populate script and readme stuffs

---
+++ public/bdd/populate_mongodb.js
@@ -0,0 +1,142 @@
+
+
+{
+    "question" : {
+    "intitule" : "Aristide Briand, né le 28 mars 1862 à Nantes1 et mort le 7 mars 1932 à Paris, est un homme politique et diplomate français qui a été onze fois Président du Conseil et vingt fois ministre. Il a joué un rôle essentiel dans les relations internationales après la Première Guerre mondiale. Il reçoit le Prix Nobel de la paix en 1926, conjointement avec Gustav Stresemann, pour son action en faveur de la réconciliation entre la France et l'Allemagne (accords de Locarno, 1925). En 1928, poursuivant son rêve de paix par la sécurité collective, dont l'axe serait la Société des Nations",
+        "wikipediaUrl" : "https://fr.wikipedia.org/wiki/Aristide_Briand",
+        "removeTerms" : [
+        "Aristide",
+        "Briand"
+    ],
+        "answers" : [
+        "Place Maréchal Foch",
+        "Place Aristide Briand",
+        "Rue Ursule Chevalier"
+    ]
+},
+    "answer" : {
+    "id" : 1,
+        "link" : "XXX",
+        "streetView" : "YYY"
+}
+}
+{
+    "question" : {
+    "intitule" : "Christophe Louis Léon Juchault de Lamoricière (ou La Moricière), né le 5 février 1806 à Nantes1 et mort le 11 septembre 1865 à Prouzel (Somme), est un officier et homme politique français. Promu général de division en 1843, au cours des campagnes coloniales dirigées par le général Bugeaud contre l'émir Abd el-Kader en Algérie, Lamoricière s'investit également dans le domaine politique et se fait élire à l'Assemblée législative. Il est nommé ministre de la Guerre en 1848, et participe activement à la répression des Journées de Juin. Exilé au cours des années 1850 à cause de son opposition à Louis-Napoléon Bonaparte, il revient en France puis se met en 1860 au service de la cause pontificale dans la lutte contre Garibaldi, jusqu'à la défaite de Castelfidardo.",
+        "wikipediaUrl" : "https://fr.wikipedia.org/wiki/Christophe_Louis_L%C3%A9on_Juchault_de_Lamorici%C3%A8re",
+        "removeTerms" : [
+        "Juchault",
+        "Lamoricière",
+        "Moricière"
+    ],
+        "answers" : [
+        "Rue Lamoricière",
+        "Rue Alexandre Dumas",
+        "Rue Marc Sangnier"
+    ]
+},
+    "answer" : {
+    "id" : 0,
+        "link" : "XXX",
+        "streetView" : "YYY"
+}
+}
+{
+    "question" : {
+    "intitule" : "Charles Monselet, né à Nantes le 30 avril 1825 et mort à Paris le 19 mai 1888, est un écrivain épicurien, journaliste, romancier, poète et auteur dramatique français, surnommé « le roi des gastronomes » par ses contemporains. Il est avec Grimod de la Reynière, le Baron Brisse et Joseph Favre l'un des premiers journalistes gastronomiques.",
+        "wikipediaUrl" : "https://fr.wikipedia.org/wiki/Charles_Monselet",
+        "removeTerms" : [
+        "Monselet",
+        "Monselet"
+    ],
+        "answers" : [
+        "Rue Charles Monselet",
+        "Boulevard Van Iseghem",
+        "Rue Alfred de Musset"
+    ]
+},
+    "answer" : {
+    "id" : 0,
+        "link" : "XXX",
+        "streetView" : "YYY"
+}
+}
+{
+    "question" : {
+    "intitule" : "Auguste Pageot, né le 22 juillet 1884 et mort le 19 novembre 1962 à Nantes, est un homme politique français, maire socialiste de Nantes de 1935 à 1940, député de Loire-Inférieure de 1936 à 1940. ",
+        "wikipediaUrl" : "https://fr.wikipedia.org/wiki/Auguste_Pageot",
+        "removeTerms" : [
+        "Auguste",
+        "Pageot"
+    ],
+        "answers" : [
+        "Rue Maréchal Joffre",
+        "Rue Corneille",
+        "Boulevard Auguste Pageot"
+    ]
+},
+    "answer" : {
+    "id" : 2,
+        "link" : "XXX",
+        "streetView" : "YYY"
+}
+}
+{
+    "question" : {
+    "intitule" : "Jules Verne, né le 8 février 1828 à Nantes en France et mort le 24 mars 1905 à Amiens en France, est un écrivain français dont l'œuvre est, pour la plus grande partie, constituée de romans d'aventures et de science-fiction (ou d'anticipation). En 1863 paraît chez l'éditeur Pierre-Jules Hetzel (1814-1886) son premier roman Cinq semaines en ballon, qui connaît un très grand succès y compris à l'étranger. Lié à l'éditeur par un contrat de vingt ans, Jules Verne travaillera en fait pendant quarante ans à ses Voyages extraordinaires",
+        "wikipediaUrl" : "https://fr.wikipedia.org/wiki/Jules_Verne",
+        "removeTerms" : [
+        "Jules",
+        "Verne"
+    ],
+        "answers" : [
+        "Rue Beethoven",
+        "Rue Alphonse Daudet",
+        "Boulevard Jules Verne"
+    ]
+},
+    "answer" : {
+    "id" : 2,
+        "link" : "XXX",
+        "streetView" : "YYY"
+}
+}
+{
+    "question" : {
+    "intitule" : "Pierre1 Marie René Ernest Waldeck-Rousseau, né à Nantes (Loire-Atlantique) le 2 décembre 1846 et mort à Corbeil-Essonnes le 10 août 1904, est un homme d'État libéral français. Il est célèbre pour avoir participé à la légalisation des syndicats (loi Waldeck-Rousseau de 1884) ainsi que la loi 1901 sur les associations. En juin 1899, il est appelé par le président Émile Loubet à former un gouvernement alors que l'Affaire Dreyfus bat son plein. ",
+        "wikipediaUrl" : "https://fr.wikipedia.org/wiki/Pierre_Waldeck-Rousseau",
+        "removeTerms" : [
+        "Waldeck",
+        "Rousseau"
+    ],
+        "answers" : [
+        "Place Waldeck Rousseau",
+        "Rue Colette Audry",
+        "Rue Urvoy de Saint Bedan"
+    ]
+},
+    "answer" : {
+    "id" : 0,
+        "link" : "XXX",
+        "streetView" : "YYY"
+}
+}
+{
+    "question" : {
+    "intitule" : "Émile Henry Mellinet, né le 1er juin 1798 à Nantes et mort le 20 janvier 1894 dans la même ville, est un militaire français, général à partir de 1850. Il prend définitivement sa retraite à Nantes par décret du 1er septembre 18782. Il devient l'une des figures les plus populaires de la vie locale : il favorise l'épanouissement des Arts et Lettres, joue au théâtre de Compiègne ; passionné de musique, il contribue à l'organisation des musiques régimentaires et compose quelques morceaux qui sont très goûtés des Nantais ; bibliophile, il donne au ministère de la Guerre une importante collection d'ouvrages militaires et, à la bibliothèque de Nantes, un fonds précieux d'autographes",
+        "wikipediaUrl" : "https://fr.wikipedia.org/wiki/%C3%89mile_Mellinet",
+        "removeTerms" : [
+        "Mellinet"
+    ],
+        "answers" : [
+        "Place Maréchal Foch",
+        "Boulevard Alexander Fleming",
+        "Place Général Mellinet"
+    ]
+},
+    "answer" : {
+    "id" : 2,
+        "link" : "XXX",
+        "streetView" : "YYY"
+}
+}
